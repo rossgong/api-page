8aefcc6593e954348eb5e256a86463aff3849218
Ross Gongaware <rossgong@gmail.com>
2018-11-27 17:53:50 -0500

    added some bells and whistles

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,29 +7,60 @@ var giphy = {
 	apiKey: "dc6zaTOxFJmzC",
 
 	populateImages: function (topic) {
-		var imageContainer = $("#image-container").html("");
+		var imageContainer = $("#image-container");
 		$.ajax({
 			url: this.constructURL(topic),
 			method: "GET"
 		}).then(function (response) {
 			var data = response.data;
+			console.log(data);
 
 			data.forEach(gif => {
-				var gifDiv = $("<div>", {
+				var imageTile = $("<div>", {
 					class: "image-tile",
 					"data-still": gif.images.fixed_height_still.url,
 					"data-animated": gif.images.fixed_height.url,
 					"data-state": "still"
 				});
-				gifDiv.append($("<img>", { src: gif.images.fixed_height_still.url }));
-				gifDiv.append($("<div>", { class: "gif-rating-div" }).text(gif.rating.toUpperCase()))
 
-				imageContainer.append(gifDiv);
+				//gifContainer contins the gif and any overlays
+				var gifContainer = $("<div>", { class: "gif-container" });
+				gifContainer.append($("<img>", { src: gif.images.fixed_height_still.url }));
+				gifContainer.append($("<div>", { class: "gif-overlay gif-rating-div" }).text(gif.rating.toUpperCase()));
+
+				//I have to dynamically set the width with the CSS to amke it display properly. I like this solution more than the table solution
+				//and it is more resiliant than the <figure> solution also used it to provide an ability to show/hide title on hover.
+
+				gifContainer.append($("<div>", { class: "gif-title-div gif-overlay" }).text(gif.title)
+					.css({ width: gif.images.fixed_height_still.width, opacity: 0 }));
+
+				imageTile.append(gifContainer);
+
+				if (gif.source_tld === "") {
+					gif.source_tld = "Not Available";
+				}
+
+				imageTile.append($("<div>", { class: "gif-src-div" }).text("Source: " + gif.source_tld));
+				imageTile.append($("<a>", { class: "gif-dl-btn btn", href: gif.images.original.url.replace(/media[0-9]/i, "i"), target: "_blank" }).text("Source"));
+				imageContainer.prepend(imageTile);
 			});
 
-			$(".image-tile").on("click", function (event) {
-				giphy.toggleAnimation($(this));
+			//Hover function shows title on mouseenter and rehides on mouseleave
+			$(".image-tile").on({
+				click: function (event) {
+					giphy.toggleAnimation($(this));
+				},
+
+				mouseenter: function (event) {
+					$(this).children().children(".gif-title-div").css({ opacity: .75 });
+				},
+
+				mouseleave: function (event) {
+					$(this).children().children(".gif-title-div").css({ opacity: 0 });
+				}
 			});
+
+
 		});
 	},
 
@@ -41,11 +72,11 @@ var giphy = {
 		if (div.attr("data-state") === "still") {
 			div.attr("data-state", "animated");
 
-			div.children("img").attr("src", div.attr("data-animated"));
+			div.children().children("img").attr("src", div.attr("data-animated"));
 		} else if (div.attr("data-state") === "animated") {
 			div.attr("data-state", "still");
 
-			div.children("img").attr("src", div.attr("data-still"));
+			div.children().children("img").attr("src", div.attr("data-still"));
 		}
 	}
 
@@ -56,8 +87,6 @@ function populateButtons() {
 	var buttonContainer = $("#button-container").html("");
 	topics.forEach(topic => {
 		var button = $("<button>", { class: "topic-button btn" }).text(topic);
-
-		console.log(topic);
 
 		buttonContainer.append(button);
 	});
